68ef9bc1019cb222afb030aa27b941638b4637ec
atousa <atoosa.havaseli@gmail.com>
2020-07-30 20:52:10 +0430

    function fixed.

--- src/test.java
+++ src/test.java
@@ -1,6 +1,20 @@
+
 public class test {
-    public static void main(String[] args) {
-        String str = "40.56";
-        System.out.println(Float.valueOf(str));
+    public static void main(String[] var0) {
+        start();
     }
+
+    static {
+    }
+
+    public static int f(int var0) {
+        return var0 == 1 ? 1 : f(var0 - 1) * var0;
+    }
+
+    public static void start() {
+        byte var0 = 3;
+        System.out.println(f(var0));
+    }
+
+
 }
